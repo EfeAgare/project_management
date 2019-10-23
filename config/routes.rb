37446d39695f44bb34f47f36f9903f127c6cae37
Efe <knowledgeagare157@gmail.com>
2019-10-23 17:03:20 +0100

    edit devise to user contollers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'registrations',
+    confirmations: 'confirmations'  # confirmations controller
+  }
   root to: "home#index"
+
+  as :user do
+    put '/user/confirmation', to: 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
