b8137da97151050a131a84224735e559df87a2dd
Efe <knowledgeagare157@gmail.com>
2019-10-24 10:56:45 +0100

    modify the teams controller

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,6 @@
 class Team < ApplicationRecord
+  has_many :users
+  has_many :projects, class_name: "project", dependent: :destroy
+
+  accepts_nested_attributes_for :users, allow_destroy: true
 end
