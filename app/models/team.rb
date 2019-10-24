a20cc9e376f5634877b9ef8608c98d4a327ddbfd
Efe <knowledgeagare157@gmail.com>
2019-10-24 18:14:37 +0100

    complete project

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,6 @@ class Team < ApplicationRecord
   has_many :projects, dependent: :destroy
 
   accepts_nested_attributes_for :users, allow_destroy: true
+  include PublicActivity::Model
+	tracked owner: Proc.new { |controller, model| controller.current_user }
 end
